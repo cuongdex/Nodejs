654f708afae82a491b62abee9fb2f981f3de9714
cuongdex <sonvancuong2003@gmail.com>
2024-09-17 15:46:40 +0700

    aa

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ const express = require("express");
 
 const cors = require("cors");
 
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 const contactsRouter = require("./app/routes/contact.route")
+
+
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts",contactsRouter);
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Sever Error",
+    });
+});
+
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not foud"))
+})
 
 app.get("/",(req, res) => {
     res.json({message: "Hello"});
 });
 
-app.use("/api/contacts",contactsRouter);
 
 module.exports = app;
